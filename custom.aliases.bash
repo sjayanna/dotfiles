37a27a83ce2b868cac520f79607819670b84716c
Sid Jayanna <sjayanna@gmail.com>
2016-09-12 00:56:33 -0400

    Add custom aliases for bash_it

---
+++ custom.aliases.bash
@@ -0,0 +1,5 @@
+# Custom aliases
+
+alias gfc='cd /Users/jayannsd/Dropbox/GFC/new/supportGFC;pwd'        # Go to GFC project folder
+alias src='cd /Users/jayannsd/src;pwd'
+alias work='cd /Users/jayannsd/work;pwd'
