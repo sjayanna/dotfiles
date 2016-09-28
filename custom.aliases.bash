0814e2b0243e3ddde9a4871fa0839481ba7887d5
Sid Jayanna <sjayanna@gmail.com>
2016-09-27 22:26:58 -0400

    Update aliases

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -1,6 +1,7 @@
 # Custom aliases
 
-alias gfc='cd $HOME/Dropbox/GFC/new/supportGFC;pwd'        # Go to GFC project folder
 alias src='cd $HOME/src;pwd'
 alias work='cd $HOME/work;pwd'
 alias gitset='git config --local user.email "sjayanna@gmail.com"'
+alias gr='git remote -v'
+alias gdiff='git diff'
