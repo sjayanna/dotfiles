bcc8da00158e1342d82de071906b095bd295fd00
Sid Jayanna <sid.jayanna@gmail.com>
2019-04-23 23:37:44 -0400

    Update aliases

--- bash_it/custom.aliases.bash
+++ bash_it/custom.aliases.bash
@@ -11,8 +11,6 @@ alias gdiff='git diff'
 alias gfetch='git fetch --prune'
 alias gamend='git commit --amend -m'
 alias gcurrent='git rev-parse --abbrev-ref HEAD'
-alias cdtxn='cd $HOME/work/connect/components/transaction;pwd'
-alias connect='cd $HOME/work/connect;pwd'
 alias bet='bundle exec ruby -Ilib:test'
 alias cdweb='cd $HOME/work/connect/components/connect;pwd'
 #alias 'rm -rf'='rm -rfi'
@@ -20,3 +18,18 @@ alias es='exercism submit'
 alias gfc='cd $HOME/work/supportGFC;rvm use 2.3.1@supportGFC;pwd'
 alias dotfiles='cd $HOME/src/dotfiles;pwd'
 alias gfc_deploy='bundle exec cap production deploy'
+# Define aliases
+alias cdca="cd ~/Sandbox/connect/apps/consumer_action"
+alias cdapi="cd ~/Sandbox/connect/apps/real_time_api"
+alias cdcommon="cd ~/Sandbox/connect/components/common"
+alias cdconnect="cd ~/Sandbox/connect/components/connect"
+alias cddeploy="cd ~/Sandbox/connect/deploy"
+alias cdjobs="cd ~/Sandbox/connect/components/jobs"
+alias cdloans="cd ~/Sandbox/connect/components/loans"
+alias cdroot="cd ~/Sandbox/connect"
+alias cdsetup="cd ~/Sandbox/claritysetup"
+alias cdrt="cd ~/Sandbox/connect/apps/real_time_api"
+
+alias be="bundle exec"
+alias betest="RAILS_ENV=test be ruby  -I\"lib\" -I\"test\""
+alias bert="RAILS_ENV=test be ruby  -I\"lib\" -I\"test\""
